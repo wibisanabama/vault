1cdc58764751fc5389f4080ed376a7789d9009f4
wibisanabama <wibisanabama@gmail.com>
2026-08-15 09:01:10 +0700

    fix(ui): add logo favicon to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
